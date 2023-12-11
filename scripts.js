9dfda748f62155d13411c547afe16e04ed868cc4
Mike-Benn <mbenn49@wgu.edu>
2023-12-11 05:38:10 -0500

    Add drag,click events to change background color of squares

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ const pageContainer = document.createElement('div');
 pageContainer.classList.add('page-container');
 document.body.appendChild(pageContainer);
 
-for (let heightIndex = 0; heightIndex < 16; heightIndex++) {
+for (let heightIndex = 0; heightIndex < 24; heightIndex++) {
     let container = document.createElement('div');
     container.classList.add('grid');
-    for(let widthIndex = 0; widthIndex < 16; widthIndex++) {
+    for(let widthIndex = 0; widthIndex < 24; widthIndex++) {
         let div = document.createElement('div');
         div.classList.add('square');
         container.appendChild(div);
@@ -13,10 +13,26 @@ for (let heightIndex = 0; heightIndex < 16; heightIndex++) {
             pageContainer.appendChild(container);
         }
 
-
     }
 
 }
+
+const squares = document.querySelectorAll('.square');
+
+
+squares.forEach(square => {
+    square.addEventListener('click' , () => {
+        square.style.backgroundColor = "black";
+    })
+})
+
+squares.forEach(square => {
+    square.addEventListener('dragover' , () => {
+        square.style.backgroundColor = "black";
+    })
+})
+
+
 
 
 
