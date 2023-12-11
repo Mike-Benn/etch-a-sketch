a6f9fcb514c765dfbc27b055ca69e21adfd18e63
Mike-Benn <mbenn49@wgu.edu>
2023-12-11 10:21:26 -0500

    Add drag and draw effect to mousedown on each square

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 const pageContainer = document.createElement('div');
 pageContainer.classList.add('page-container');
+pageContainer.setAttribute('draggable' , 'false');
 document.body.appendChild(pageContainer);
 
 for (let heightIndex = 0; heightIndex < 24; heightIndex++) {
     let container = document.createElement('div');
     container.classList.add('grid');
+    container.setAttribute('draggable' , 'false');
     for(let widthIndex = 0; widthIndex < 24; widthIndex++) {
         let div = document.createElement('div');
         div.classList.add('square');
+        div.setAttribute('draggable' , 'false');
         container.appendChild(div);
         if (widthIndex == 15) {
             pageContainer.appendChild(container);
@@ -18,22 +21,76 @@ for (let heightIndex = 0; heightIndex < 24; heightIndex++) {
 }
 
 const squares = document.querySelectorAll('.square');
+let mouseDown = false;
+document.body.addEventListener('mouseup' , toggleMouse);
+function toggleMouse() {
+    mouseDown = false;
+    console.log("mouse up");
+    squares.forEach(square => {
+        square.removeEventListener('mouseenter' , blackSquare);
+        console.log("squares removed");
+    })
+}
+
+function blackSquare(event) {
+    event.target.style.backgroundColor = "black";
+
+}
+
 
 
 squares.forEach(square => {
-    square.addEventListener('click' , () => {
-        square.style.backgroundColor = "black";
+    square.addEventListener('mousedown' , blackSquare);
+})
+
+squares.forEach(square => {
+    square.addEventListener('mousedown' , () => {
+        mouseDown = true;
+        squares.forEach(square => {
+            square.addEventListener('mouseenter' , blackSquare);
+        })
     })
 })
 
 squares.forEach(square => {
-    square.addEventListener('dragover' , () => {
-        square.style.backgroundColor = "black";
+    square.addEventListener('mouseup' , toggleMouse)})
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('dragstart', function(e) {
+        e.preventDefault();
+        return false;
+    });
+});
+
+
+/*
+squares.forEach(square => {
+    square.addEventListener('mousedown' , () => {
+        square.addEventListener('mousehover' , () => {
+            square.style.backgroundColor = "black";
+        })
+        square.addEventListener('mouseup' , () => {
+            square.removeEventListener('mousehover')
+        })
     })
 })
 
+squares.forEach(square => {
+    square.addEventListener('mousedown' , continuousFire)
+    
+});
 
 
+squares.forEach(square => {
+    square.addEventListener('mouseup' , () => {
+        mouseDown = false;
+        console.log("mouse up");
+    })
+})
+*/
 
 
 
