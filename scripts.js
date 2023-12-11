0ee9812be96a9029120476cd00ac44b97b469eeb
Mike-Benn <mbenn49@wgu.edu>
2023-12-11 04:46:23 -0500

    Change variable names,remove hover effect

--- scripts.js
+++ scripts.js
@@ -1,15 +1,24 @@
-const dimension = 16;
+const pageContainer = document.createElement('div');
+pageContainer.classList.add('page-container');
+document.body.appendChild(pageContainer);
 
 for (let heightIndex = 0; heightIndex < 16; heightIndex++) {
     let container = document.createElement('div');
+    container.classList.add('grid');
     for(let widthIndex = 0; widthIndex < 16; widthIndex++) {
         let div = document.createElement('div');
+        div.classList.add('square');
         container.appendChild(div);
         if (widthIndex == 15) {
-            document.body.appendChild(container);
+            pageContainer.appendChild(container);
         }
 
 
     }
 
 }
+
+
+
+
+
