3d5b18c49963df07d835446c7ca01cfa3f7d57de
Mike-Benn <mbenn49@wgu.edu>
2023-12-14 08:49:46 -0500

    Add toggle buttons for every grid control Add on and off effect to every button

--- scripts.js
+++ scripts.js
@@ -1,13 +1,31 @@
-
 const grid = document.querySelector('.grid');
 const slider = document.querySelector('#size-slider');
 const sizeValue = document.querySelectorAll('.grid-size');
 const singleSizeValue = document.querySelector('.grid-size');
+const fillBtn = document.querySelector('#fill-button');
+const pickBtn = document.querySelector('#picker-button');
 const clearBtn = document.querySelector('#clear');
 const gridLinesBtn = document.querySelector('#grid-lines');
+const foregroundColor = document.querySelector('#forecolor');
+const backgroundColor = document.querySelector('#backcolor');
+const eraserBtn = document.querySelector('#eraser');
+const prismaticBtn = document.querySelector('#prismatic');
+const darkenBtn = document.querySelector('#darken');
+const brightenBtn = document.querySelector('#brighten');
+
 let squares = document.querySelectorAll('.square');
 let mouseDown = false;
+
+// Button status variables
+
 let gridLines = true;
+let fillSelected = false;
+let pickerSelected = false;
+let eraserSelected = false;
+let prismaticSelected = false;
+let darkenSelected = false;
+let brightenSelected = false;
+
 makeGrid();
 
 
@@ -80,7 +98,7 @@ function toggleMouse() {
 // Sets event target to the background color of black
 
 function blackSquare(event) {
-    event.target.style.backgroundColor = "black";
+    event.target.style.backgroundColor = foregroundColor.value;
 
 }
 
@@ -117,6 +135,116 @@ function removeMouseEnter() {
 
 removeMouseEnter();
 
+// Selects or deslects the color fill tool
+
+function fillToggle() {
+    fillBtn.addEventListener('click' , () => {
+        if (fillSelected) {
+            fillSelected = false;
+            fillBtn.style.color = "#9fd3c7";
+            fillBtn.style.backgroundColor = "#385170";
+        } else {
+            fillSelected = true;
+            fillBtn.style.color = "#385170";
+            fillBtn.style.backgroundColor = "#9fd3c7";
+        }
+    })
+}
+
+fillToggle();
+
+// Selects or deselects the color picker tool
+
+function colorPickerToggle() {
+    pickBtn.addEventListener('click' , () => {
+        if (pickerSelected) {
+            pickerSelected = false;
+            pickBtn.style.color = "#9fd3c7";
+            pickBtn.style.backgroundColor = "#385170";
+        } else {
+            pickerSelected = true;
+            pickBtn.style.color = "#385170";
+            pickBtn.style.backgroundColor = "#9fd3c7";
+            
+        }
+    })
+}
+
+colorPickerToggle();
+
+// Selects or deselects the eraser tool
+
+function eraserToggle() {
+    eraserBtn.addEventListener('click' , () => {
+        if (eraserSelected) {
+            eraserSelected = false;
+            eraserBtn.style.color = "#9fd3c7";
+            eraserBtn.style.backgroundColor = "#385170";
+        } else {
+            eraserSelected = true;
+            eraserBtn.style.color = "#385170";
+            eraserBtn.style.backgroundColor = "#9fd3c7";
+        }
+    })
+    
+}
+
+eraserToggle();
+
+// Selects or deselects the prismatic tool
+
+function prismaticToggle() {
+    prismaticBtn.addEventListener('click' , () => {
+        if (prismaticSelected) {
+            prismaticSelected = false;
+            prismaticBtn.style.color = "#9fd3c7";
+            prismaticBtn.style.backgroundColor = "#385170";
+        } else {
+            prismaticSelected = true;
+            prismaticBtn.style.color = "#385170";
+            prismaticBtn.style.backgroundColor = "#9fd3c7";
+        }
+    })
+}
+
+prismaticToggle();
+
+// Selects or deselects the darken tool
+
+function darkenToggle() {
+    darkenBtn.addEventListener('click' , () => {
+        if (darkenSelected) {
+            darkenSelected = false;
+            darkenBtn.style.color = "#9fd3c7";
+            darkenBtn.style.backgroundColor = "#385170";
+        } else {
+            darkenSelected = true;
+            darkenBtn.style.color = "#385170";
+            darkenBtn.style.backgroundColor = "#9fd3c7";
+        }
+    })
+}
+
+darkenToggle();
+
+// Selects or deselects the brighten tool
+
+function brightenToggle() {
+    brightenBtn.addEventListener('click' , () => {
+        if (brightenSelected) {
+            brightenSelected = false;
+            brightenBtn.style.color = "#9fd3c7";
+            brightenBtn.style.backgroundColor = "#385170";
+        } else {
+            brightenSelected = true;
+            brightenBtn.style.color = "#385170";
+            brightenBtn.style.backgroundColor = "#9fd3c7";
+        }
+    })
+}
+
+brightenToggle();
+
 // Toggles grid lines on and off
 
 function toggleGridLines() {
@@ -138,12 +266,13 @@ function toggleGridLines() {
 }
 
 toggleGridLines();
+
 // Sets all squares to the current background color
 
 function clearGrid() {
     clearBtn.addEventListener('click' , () => {
         squares.forEach(square => {
-            square.style.backgroundColor = "white";
+            square.style.backgroundColor = backgroundColor.value;
         })
     })    
 }
